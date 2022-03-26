71cf0641d5b2659ad2670a498ef46e71c70e13c4
robertMihai19 <74791527+robertMihai19@users.noreply.github.com>
2022-03-26 16:20:22 +0200

    COmmit

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose");
 const env = require("./config");
+const axios = require("axios");
+const company = require("./models/company");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,6 +13,20 @@ mongoose.connect(env.mongo_path, () => {
     console.log("Mongoose Initialized");
 })
 
+// axios.get(env.db_path).then(res => {
+//     console.log(res.data)
+// });
+
+// const gigel = async () => {
+//     await company.create({
+//         name: "Marcel",
+//         factors: [{key: "das", value: [1, 2]}, {key: "dasdas", value: [1, 3, 4]}],
+//         category: ["Plm"]
+//     });
+// }
+// gigel();
+
 app.listen(env.port, () => {
+
     console.log("Listening on port 5000");
 })
