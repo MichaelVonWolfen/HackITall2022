5a31e080f0269bf1c2e36e1b36bd57e497633521
Mihai Stoica <41210245+MichaelVonWolfen@users.noreply.github.com>
2022-03-26 22:18:02 +0200

    Added first graph

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import SearchBar from "./components/helpers/SearchBar/searchBar";
 import Navbar from "./components/Navbar/navbar";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Charts from "./components/ChartsThing/Charts";
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<div>Hello Home</div>}/>
+                <Route path="/charts" element={<Charts/>} />
+                <Route path="/about" element={<div/>} />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
