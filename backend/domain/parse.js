5067d42ad5ba9dfd23e214e9ce79983b7857b408
robertMihai19 <74791527+robertMihai19@users.noreply.github.com>
2022-03-26 16:59:57 +0200

    Commit

---
+++ backend/domain/parse.js
@@ -0,0 +1,10 @@
+const companies = require("./seeder")();
+const company = require("./../models/company");
+
+const parseCompany = async () => {
+    companies.forEach()
+}
+
+module.exports = {
+    parseCompany
+};
