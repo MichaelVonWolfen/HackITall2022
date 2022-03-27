2b5e5f9c53f75b1fc8de570ded15223e31c4de71
Mihai Stoica <41210245+MichaelVonWolfen@users.noreply.github.com>
2022-03-27 10:11:58 +0300

    Added search COntainer

--- frontend/src/components/helpers/SearchBar/searchBar.tsx
+++ frontend/src/components/helpers/SearchBar/searchBar.tsx
@@ -1,22 +1,69 @@
 import React, {useState} from 'react';
 import "./searchBar.sass"
-import HamburgerIcon from "../HamburgerIcon/HamburgerIcon";
+import config from "../../../config";
 
-interface PLM{
+interface Viata{
     dataSetter:any
 }
-export default function SearchBar(props:PLM){
+export default function SearchBar(props:Viata){
+    const [companiesList, setCompaniesList] = useState([])
     const handleChange = (e:any) => {
         e.preventDefault()
-        let val = document.getElementById("searchbar")
-        if(!val) return
         // @ts-ignore
+        let val = document.getElementById("searchbar").value
+        if(!val) return
         props.dataSetter(val.value)
+    }
+    const handleInput = (e:any) => {
+        // @ts-ignore
+        let val = document.getElementById("searchbar").value
+        if(!val) return
+        // console.log(val)
+        if(val.length > 0){
+            // @ts-ignore
+            document.getElementById("searchItemsList").classList.remove("hidden")
+            let dataList: JSX.Element[] = []
+            config.companies.forEach(company =>{
+                if(company.company.toLowerCase().includes(val.toLowerCase()) || company.stock_ID.toLowerCase().includes(val.toLowerCase())){
+                    dataList.push(
+                        <div className="searchItem" onClick={() =>{
+                            props.dataSetter(company.stock_ID)
+                            let itemList = document.getElementById("searchItemsList")
+                            itemList && itemList.classList.add("hidden")
+                            let searchbar = document.getElementById("searchbar")
+                            // @ts-ignore
+                            if(searchbar) searchbar.value = "";
+
+                        }}>
+                            <span className="searchItemBig">{company.company}</span>
+                            <span className="searchItemSmall">{company.stock_ID}</span>
+                        </div>
+                    )
+                }
+            })
+            // @ts-ignore
+            setCompaniesList([...dataList])
+        }
+    }
+    const handleKeyPress =(e:any) =>{
+        console.log()
+        setTimeout(()=>{
+            let input = document.querySelector(".searchBarContainer #searchbar")
+            let itemList = document.getElementById("searchItemsList")
+                // @ts-ignore
+            if(e.keyCode === 8 && input.value.length === 0){
+                // @ts-ignore
+                itemList.classList.add("hidden")
+            }
+        }, 200)
     }
     return(
         <form className={"searchBarContainer"} onSubmit={handleChange}>
-            <input type="text" name="searchbar" id="searchbar" placeholder={"Search..."}/>
+            <input type="text" name="searchbar" id="searchbar" placeholder={"Search..."} onInput={handleInput} onKeyDown={handleKeyPress}/>
             <button type="submit">Search</button>
+            <div className="searchItemsList hidden" id={"searchItemsList"}>
+                {companiesList}
+            </div>
         </form>
     )
 }
---
+++ frontend/src/config.tsx
@@ -0,0 +1,35 @@
+export default Object.freeze({
+    companies:[
+        {
+            stock_ID:"AAPL",
+            company:"Apple"
+        }, {
+            stock_ID:"MSFT",
+            company:"Microsoft"
+        }, {
+            stock_ID:"GOOGL",
+            company:"Google"
+        }, {
+            stock_ID:"AMZN",
+            company:"Amazon"
+        }, {
+            stock_ID:"FB",
+            company:"Facebook (or Meta 😑)"
+        }, {
+            stock_ID:"TSM",
+            company:"Taiwan Semiconductor"
+        }, {
+            stock_ID:"NVDA",
+            company:"NVidia"
+        }, {
+            stock_ID:"TSLA",
+            company:"Tesla"
+        }, {
+            stock_ID:"JNJ",
+            company:"Johnson & Johnson"
+        }, {
+            stock_ID:"PG",
+            company:"P&G"
+        },
+    ]
+})
