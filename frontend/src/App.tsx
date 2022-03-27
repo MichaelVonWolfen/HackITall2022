a42827cd7ab52d286a39b6c74b173a5b7d47a3cc
Mihai Stoica <41210245+MichaelVonWolfen@users.noreply.github.com>
2022-03-27 02:19:50 +0200

    YOu can search for more data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,26 @@
 import Navbar from "./components/Navbar/navbar";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import ChartsPage from "./components/ChartsPage/CharttsPage";
+import {useState} from "react";
 
+interface CePLM{
+    data:any,
+    dataSet:any
+}
 function App() {
-  return (
-    <div className="App">
-        <Navbar/>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<div>Hello Home</div>}/>
-                <Route path="/charts" element={<ChartsPage/>} />
-                <Route path="/about" element={<div/>} />
-            </Routes>
-        </BrowserRouter>
-    </div>
-  );
+    const[searchInput, setSearchInput] = useState("")
+    return (
+        <div className="App">
+            <Navbar dataSetter={setSearchInput}/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<div>Hello Home</div>}/>
+                    <Route path="/charts" element={<ChartsPage data={searchInput}/>} />
+                    <Route path="/about" element={<div/>} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
